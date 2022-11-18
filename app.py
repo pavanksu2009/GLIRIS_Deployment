7a9d42c31c08778dc02721b037d2db39eaeb993c
pavanksu2009 <pavankumar.ch7@gmail.com>
2022-11-18 11:04:08 +0000

    This is commit for webapp

---
+++ app.py
@@ -0,0 +1,26 @@
+# Import libraries
+import numpy as np
+from flask import Flask, request, jsonify, json
+import pickle
+import os
+# Load the model
+model = pickle.load(open('gliris.pkl','rb'))
+app = Flask(__name__)
+@app.route('/api',methods=['POST'])
+def predict():
+    # Get the data from the POST request.
+    # data=json.dumps({'sl':3.2,'sw':7.3,'pl':4.5,'pw':2.1})
+    data = request.json['data']
+    # Make prediction using model loaded from disk as per the data.
+    predict_request=[[data['sl'],data['sw'],data['pl'],data['pw']]]
+    predict_request=np.array(predict_request)
+    print(predict_request)
+    prediction = model.predict(predict_request)
+    print(prediction)
+    # Take the first value of prediction
+    output = prediction[0]
+    print(output)
+    return jsonify(int(output))
+
+if __name__ == '__main__':
+    app.run(debug=True)
